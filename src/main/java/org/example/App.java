53fe594814542a423bc2b1dfae08458e23963082
Giga Tsitsagi <gigatsitsagi2@gmail.com>
2025-10-08 16:25:20 +0400

    rename

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -30,10 +30,6 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.function.*;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) throws Exception {
 
